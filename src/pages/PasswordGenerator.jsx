e5ca44b4b9b07e37f10554de4dd9345751e6cfbf
Fioo1 <171174808+Fioo1@users.noreply.github.com>
2026-06-29 10:55:38 -0500

    cambio en la interfaz password generator

--- src/pages/PasswordGenerator.jsx
+++ src/pages/PasswordGenerator.jsx
@@ -1,30 +1,132 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function PasswordGenerator() {
+
+  const [password, setPassword] = useState("");
+
+  const [length, setLength] = useState(16);
+
+  const [uppercase, setUppercase] = useState(true);
+
+  const [lowercase, setLowercase] = useState(true);
+
+  const [numbers, setNumbers] = useState(true);
+
+  const [symbols, setSymbols] = useState(true);
+
   return (
     <main>
 
       <Link
         to="/"
-        style={{
-          color:"#60a5fa",
-          textDecoration:"none",
-          fontSize:"18px"
-        }}
+        className="back-link"
       >
         ← Back
       </Link>
 
-      <h1 style={{marginTop:"30px"}}>
-        Password Generator
-      </h1>
+      <section className="generator-container">
 
-      <p>
-        Generate secure passwords.
-      </p>
+        <h1>🔑 Password Generator</h1>
+
+        <p>
+          Generate secure and customizable passwords.
+        </p>
+
+        <input
+          type="text"
+          value={password}
+          readOnly
+          className="password-input"
+        />
+
+        <div className="length-container">
+
+          <label>
+
+            Password Length
+
+          </label>
+
+          <span>
+
+            {length}
+
+          </span>
+
+        </div>
+
+        <input
+          type="range"
+          min="8"
+          max="64"
+          value={length}
+          onChange={(e)=>setLength(e.target.value)}
+        />
+
+        <div className="options">
+
+          <label>
+
+            <input
+              type="checkbox"
+              checked={uppercase}
+              onChange={()=>setUppercase(!uppercase)}
+            />
+
+            Uppercase
+
+          </label>
+
+          <label>
+
+            <input
+              type="checkbox"
+              checked={lowercase}
+              onChange={()=>setLowercase(!lowercase)}
+            />
+
+            Lowercase
+
+          </label>
+
+          <label>
+
+            <input
+              type="checkbox"
+              checked={numbers}
+              onChange={()=>setNumbers(!numbers)}
+            />
+
+            Numbers
+
+          </label>
+
+          <label>
+
+            <input
+              type="checkbox"
+              checked={symbols}
+              onChange={()=>setSymbols(!symbols)}
+            />
+
+            Symbols
+
+          </label>
+
+        </div>
+
+        <button>
+
+          Generate Password
+
+        </button>
+
+      </section>
 
     </main>
   );
+
 }
 
 export default PasswordGenerator;
